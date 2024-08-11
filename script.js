47d340810ad09b14a5163fee36c0855cf11cd223
WareeshaMaryam <wareeshamaryam8@gmail.com>
2024-08-11 22:46:03 +0500

    Done

---
+++ script.js
@@ -0,0 +1,11 @@
+var age ="15";
+var track="14";
+var visitor="<b>John Doe";
+var product="<b>T-Shirt";
+var Quantity="<b>5";
+var birthYear="1990";
+alert("I am " + age + " years old");
+alert("You have visited this site " + track + " times");
+document.write(" My birthday year is " + birthYear +"<br/>");
+document.write("Data type of my declared variable is number" + "<br/>" + "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>" )
+document.write( visitor  + " ordered " + Quantity  +  product + "(s) on XYZ clothing store" );  s
